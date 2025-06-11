963ffb8311b09d0c8cf3e77086103cddc9dabfda
Batuhan Aktaş <52565308+batuhapro@users.noreply.github.com>
2025-06-11 13:50:26 +0300

    Update index.js

--- kickbot/index.js
+++ kickbot/index.js
@@ -1,22 +1,21 @@
-const puppeteer = require("puppeteer");
+const chromium = require('chrome-aws-lambda');
 
-const VISIT_URL = "https://garnet-volcano-apparatus.glitch.me/";
-
-async function runBot() {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+(async () => {
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless
   });
 
   const page = await browser.newPage();
-  await page.goto(VISIT_URL, { waitUntil: "networkidle2" });
+  await page.goto("https://garnet-volcano-apparatus.glitch.me/", {
+    waitUntil: 'networkidle2'
+  });
 
-  console.log("✅ Sayfa yüklendi!");
+  console.log("✅ Ziyaret tamamlandı");
 
-  await page.waitForTimeout(15000); // 15 saniye izlenme gibi dursun
+  await page.waitForTimeout(15000); // 15 saniye kal
 
   await browser.close();
-}
-
-setInterval(runBot, 2 * 60 * 1000); // her 2 dakikada bir çalıştır
-runBot();
+})();
